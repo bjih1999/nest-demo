ed20be377784d35785f5b372c057586a71052dcb
bjih1999 <bjih1999@naver.com>
2025-05-17 23:27:33 +0900

    feat: (auth) 예외 처리 필터 추가

--- apps/auth/src/app.module.ts
+++ apps/auth/src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerModule } from './logger/logger.module';
+import { APP_FILTER } from '@nestjs/core';
+import { GlobalExceptionFilter } from './common/error/exception-filter';
+import { ApplicationExceptionFilter } from './common/error/application-exception-filter';
 
 @Module({
   imports: [LoggerModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: ApplicationExceptionFilter,
+    },
+    AppService,
+  ],
 })
 export class AppModule {}
