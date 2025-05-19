8ead2aa67df0203e5e723bd10c02c43d136b68a6
bjih1999 <bjih1999@naver.com>
2025-05-19 21:32:05 +0900

    feat: (event) 예외 필터 및 로거 세팅

--- apps/event/src/app.module.ts
+++ apps/event/src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { APP_FILTER } from '@nestjs/core';
+import { GlobalExceptionFilter } from '../common/error/exception-filter';
+import { ApplicationExceptionFilter } from '../common/error/application-exception-filter';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot('mongodb://event-db:27017/event'),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: ApplicationExceptionFilter,
+    },
+    AppService,
+  ],
 })
 export class AppModule {}
---
+++ apps/event/src/common/error/error-code.ts
@@ -0,0 +1,43 @@
+import { HttpStatus } from '@nestjs/common';
+
+interface ErrorContent {
+  name: string;
+  code: string;
+  message?: string;
+  httpStatusCode: HttpStatus;
+}
+
+export const errorCode: Record<string, ErrorContent> = {
+  INVALID_INPUT_ERROR: {
+    name: 'INVALID_INPUT_ERROR',
+    code: 'A100',
+    message: '잘못된 입력값입니다.',
+    httpStatusCode: HttpStatus.BAD_REQUEST,
+  },
+  DUPLICATE_USER_ID: {
+    name: 'DUPLICATE_USER_ID',
+    message: '이미 존재하는 사용자 ID입니다.',
+    code: 'A101',
+    httpStatusCode: HttpStatus.BAD_REQUEST,
+  },
+  UNAUTHORIZED: {
+    name: 'UNAUTHORIZED',
+    message: '인증되지 않은 사용자입니다.',
+    code: 'A102',
+    httpStatusCode: HttpStatus.UNAUTHORIZED,
+  },
+  INTERNAL_SERVER_ERROR: {
+    name: 'INTERNAL_SERVER_ERROR',
+    message: '서버 내부 오류입니다.',
+    code: '901',
+    httpStatusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+  },
+  INVALID_TARGET_SERVICE: {
+    name: 'INVALID_TARGET_SERVICE',
+    message: '잘못된 target service입니다.',
+    code: '1000',
+    httpStatusCode: HttpStatus.BAD_REQUEST,
+  },
+} as const;
+
+export type ErrorCode = (typeof errorCode)[keyof typeof errorCode];
